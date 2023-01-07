8c0d87d51b48ff3cba47bd628c6dd9c485372c47
Leemon92 <e.leemon@hotmail.com>
2023-01-07 10:32:06 -0600

    070123-Employee preparacion para update

--- Rinku/Scripts/Specific/Employee.js
+++ Rinku/Scripts/Specific/Employee.js
@@ -23,7 +23,8 @@ var EmployeeActions = (function () {
             { title: "EmployeeName", field: "EmployeeName", headerSort: false, editable: true, visible: true, hozAlign: "center" },
             { title: "Rol", field: "Rol", headerSort: false, editable: true, visible: true, hozAlign: "center" },
             { title: "CreationDte", field: "CreationDte", headerSort: false, editable: true, visible: true, hozAlign: "center" },
-            { title: "Active", field: "Active", headerSort: false, editable: true, visible: true, hozAlign: "center" },
+            { title: "Status", field: "Active", headerSort: false, editable: true, visible: true, hozAlign: "center" },
+            { title: "idActive", field: "Active", headerSort: false, editable: true, visible: false },
             { title: "ID", field: "Active", headerSort: false, editable: true, visible: false },
             { title: "idRol", field: "Active", headerSort: false, editable: true, visible: false },
         ];
@@ -32,10 +33,22 @@ var EmployeeActions = (function () {
             data: datajson,
             layout: "fitColumns",
             pagination: false,
+            selectable: true,
             placeholder: "No Data Not Found",
             invalidOptionWarnings: false,
-            selectable: "highlight",
             columns: columns,
+        });
+
+        EmployeeTable.on("rowSelectionChanged", function (data, rows) {
+            if (data.length > 0) {
+                $("#txtEmployeNumber").val(String(data[0].EmployeeNumber));
+                $("#txtEmployeName").val(String(data[0].EmployeeName));
+                $("#cmbxRol").val(Number(data[0].idRol));
+                if (data[0].idActive === true) { document.getElementById('chbxActive').checked = true; }
+                else { document.getElementById('chbxActive').checked = false; }
+                EmployeeTable.deselectRow();
+                //EmployeeTable.selectRow(Number(rows[0]._row.position));
+            }
         });
     };
 
@@ -76,4 +89,59 @@ function Save_New_Employe() {
     catch (issue) {
         console.log("issue .onEmployeJS_Save_New_Employe=" + issue);
     }
+}
+
+function Save_New_Employe() {
+    try {
+        $("#modalMessage p").html('');
+        $("#modalMessage").modal("hide");
+        var idRolSelected = $("#cmbxRol").val()
+
+        if (String($("#txtEmployeName").val()).trim() == '' || String($("#txtEmployeName").val()).trim() == 'undefined') {
+            alert("Please, type employee name");
+            return;
+        }
+
+        if (Number(idRolSelected) <= 0) {
+            alert("Please, select a rol");
+            return;
+        }
+
+        if (confirm("Are you sure to save?")) {
+            var url = $("#hdnUpdEmployeeURL").val();
+
+            if (String($("#txtEmployeNumber").val()).trim() == '' || String($("#txtEmployeNumber").val()).trim().toLowerCase() == 'undefined') {
+                url = $("#hdnCreateEmployeeURL").val();
+            }
+
+            var data = {};
+            data["Number"] = Number($("#txtEmployeNumber").val());
+            data["idRol"] = Number(idRolSelected);            
+            data["Name"] = String($("#txtEmployeName").val()).trim();
+            data["Active"] = Number(1);
+
+            //CREATE EMPLOYEE
+            ajaxFormHelper(url, "POST", data, null, false)
+                .done(function (result) {
+                    $("#modalMessage p").html('<br>' + String(result.Msg) + '. <br>&nbsp;');
+                    $("#modalMessage").modal({ backdrop: "static", keyboard: false });
+                    $("#modalMessage").modal("show");
+                });
+        }
+        else
+            return false;
+    }
+    catch (issue) {
+        $("#modalMessage p").html('<br><p style="color:red">' + String(issue) + '. </p><br>&nbsp;');
+        $("#modalMessage").modal({ backdrop: "static", keyboard: false });
+        $("#modalMessage").modal("show");
+        console.log("issue .onEmployeJS_Save_New_Employe=" + issue);
+    }
+}
+
+function Cleaninfo() {
+    $("#txtEmployeNumber").val('');
+    $("#txtEmployeName").val('');
+    $("#cmbxRol").val(0)
+    document.getElementById('chbxActive').checked = false;
 }
